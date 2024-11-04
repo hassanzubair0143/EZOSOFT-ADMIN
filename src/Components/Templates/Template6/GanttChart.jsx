998b71b675f77fed95b7eae01958e3234dd67b3f
M_Hassan_Zubair <mianhassanzubair0143@gmail.com>
2024-11-04 14:19:18 +0500

    worked

--- src/Components/Templates/Template6/GanttChart.jsx
+++ src/Components/Templates/Template6/GanttChart.jsx
@@ -8,32 +8,7 @@ const GanttChart = () => {
     { name: "Task 4", start: 6, end: 12, color: "bg-grey" },
   ]; 
 
-  return (
-    <div className="w-full p-6">
-      {/* Gantt Chart Grid */}
-      <div className="grid grid-cols-12 gap-4">
-        {/* Timeline Row */}
-        <div className="grid grid-cols-12 col-span-12 mb-2">
-          {Array.from({ length: 12 }, (_, i) => (
-            <div key={i} className="font-semibold text-center border">{i + 1}</div>
-          ))}
-        </div>
 
-        {/* Task Rows */}
-        {tasks.map((task, index) => (
-          <div key={index} className="relative grid h-5 grid-cols-12 col-span-12 ">
-            <div
-              className={`${task.color} h-full rounded-md`}
-              style={{
-                gridColumnStart: task.start,
-                gridColumnEnd: task.end + 1,
-              }}
-            ></div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
 };
 
 export default GanttChart;
